794cee2ad0ef1de54a71bd38c17e3dc24f0e2834
Siddharth Sharma <105628623+Siddhu2481@users.noreply.github.com>
2022-10-11 22:00:24 +0530

    Create 876.Middle of Linked List.cpp

---
+++ 876.Middle of Linked List.cpp
@@ -0,0 +1,9 @@
+ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            
+        }
+        return slow;
