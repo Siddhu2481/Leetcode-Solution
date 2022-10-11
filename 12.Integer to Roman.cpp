61fdd204e17b78cab5db421520245a3469f6950d
Siddharth Sharma <105628623+Siddhu2481@users.noreply.github.com>
2022-10-11 22:09:16 +0530

    Create 12.Integer to Roman.cpp

---
+++ 12.Integer to Roman.cpp
@@ -0,0 +1,10 @@
+string ans = "";
+        string nos[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        for(int i = 0; num > 0; i++){
+            while(num >= val[i]){
+                num -= val[i];
+                ans += nos[i];
+            }
+        }
+        return ans;
