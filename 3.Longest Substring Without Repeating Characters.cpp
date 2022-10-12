06788142bd54142dd0b9e21f81b2813c22f0e284
Siddharth Sharma <105628623+Siddhu2481@users.noreply.github.com>
2022-10-12 23:38:13 +0530

    Create 3.Longest Substring Without Repeating Characters.cpp

---
+++ 3.Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,12 @@
+int ans = 0;
+        unordered_map<char, int> map;
+
+        for(int i = 0, j = 0; j < s.size(); j++){
+            if(map.find(s[j]) != map.end()){
+                i = max(map[s[j]], i);
+            }
+            ans = max(ans, j - i + 1);
+            map[s[j]] = j + 1;
+        }
+
+        return ans;
